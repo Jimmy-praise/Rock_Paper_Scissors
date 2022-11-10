b98c034aadad076c38708176863cf9bd4d21652c
PJimmy_McP_Small <Jimmlius923@gmail.com>
2022-11-10 14:45:41 +0100

    Created function to get computer's choice

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Get Computer Choice
 function getComputerChoice() {
-
+    let choices = ['ROCK', 'PAPER', 'SCISSORS']
+    // Get Random number
+    let randomindex = Math.floor(Math.random() * 3)
+    // Get Random item which is the computer choice
+    computerChoice = choices[randomindex]
+    return computerChoice
 };
 
 // Get Player's choice
