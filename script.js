cf3037e004a53b6bc19378137d41fb33bc37f37c
PJimmy_McP_Small <Jimmlius923@gmail.com>
2022-11-10 14:41:44 +0100

    Created function to get Player's choice

---
+++ script.js
@@ -0,0 +1,11 @@
+// Get Computer Choice
+function getComputerChoice() {
+
+};
+
+// Get Player's choice
+function getPlayerChoice() {
+    let playerChoice = prompt('Rock, Paper, Scissors', 'Rock');
+    return playerChoice.toUpperCase()
+};
+
