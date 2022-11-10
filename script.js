bbf4c496241166554278f0efd1bd51d3d4c6b6a4
PJimmy_McP_Small <Jimmlius923@gmail.com>
2022-11-10 14:47:34 +0100

    Created function playround

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ function getPlayerChoice() {
     return playerChoice.toUpperCase()
 };
 
+// PlayRound
+function playRound(computerSelection, playerSelection) {
+    // Tests if Player entered wrong input
+    if (playerSelection !== 'ROCK' && playerSelection !== 'PAPER' && playerSelection !== 'SCISSORS') {
+        console.error(`Wrong input!!! You entered ${playerSelection}. Check your spellings and try again.`)
+    }
+
+    // Determines if there is no winner
+    else if (computerSelection == playerSelection) {
+        console.log(`Ooops! This is a draw as ${computerSelection} cannot beat ${playerSelection} There is no winner, Try Again!!!`)
+    }
+
+    // Determines Winner between ROCK and SCISSORS
+    else if (
+        computerSelection == 'ROCK' && playerSelection == "SCISSORS" || computerSelection == 'SCISSORS' && playerSelection == "PAPER" || 
+        computerSelection == 'PAPER' && playerSelection == "ROCK") {
+        console.log(`Ooops! You Lose. ${computerSelection} beats ${playerSelection}.`)
+    } 
+    else {console.log(`Hooray!!! You won. ${playerSelection} beats ${computerSelection}`)}
+
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+
+console.log(`Computer's choice is ${computerSelection}`)
+console.log(`Player's choice is ${playerSelection}`)
+
+playRound(computerSelection, playerSelection)
