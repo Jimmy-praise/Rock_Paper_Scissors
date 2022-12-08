7d5d19e2320e73026d9dbe88754f78f06a3526fd
Home_Laptop <Jimmlius923@gmail.com>
2022-12-08 20:12:38 +0100

    Created playround function and added it to buttons

--- script.js
+++ script.js
@@ -15,23 +15,46 @@ function getComputerChoice() {
     return computerChoice;
 };
 
-// Get Player's choice
+function playRound(playerSelection) {
+    computerSelection = getComputerChoice();
+
+    if (computerSelection == playerSelection) {
+        resultPanel.classList.add('draw');
+        result.textContent = 'NO WINNER!';
+        analysis.textContent = `${computerSelection} cannot beat ${playerSelection}`;
+
+    }
+    else if (
+        computerSelection == 'ROCK' && playerSelection == "SCISSORS" || 
+        computerSelection == 'SCISSORS' && playerSelection == "PAPER" || 
+        computerSelection == 'PAPER' && playerSelection == "ROCK") {
+            resultPanel.classList.add('lose');
+            result.textContent = 'YOU LOSE!';
+            analysis.textContent = `${playerSelection} cannot beat ${computerSelection}`;
+    } 
+    else {
+        resultPanel.classList.add('win');
+        result.textContent = 'YOU WON!';
+        analysis.textContent = `${playerSelection} beats ${computerSelection}`;
+    }
+}
+
+
 const rock = document.querySelector('#rock')
 const paper = document.querySelector('#paper')
 const scissors = document.querySelector('#scissors')
 
-let playerChoice = '';
-
-rock.addEventListener('click', () => {
-    playerChoice = 'ROCK';
-});
-paper.addEventListener('click', () => {
-    playerChoice = 'PAPER';
-});
-scissors.addEventListener('click', () => {
-    playerChoice = 'SCISSORS';
-});
+    rock.addEventListener('click', () => {
+        playRound('ROCK')
+    });
+    paper.addEventListener('click', () => {
+        playRound('PAPER')
+    });
+    scissors.addEventListener('click', () => {
+        playRound('SCISSORS')
+    });
 
 
-
-
+const resultPanel = document.querySelector('.result-panel');
+const result = document.querySelector('#result')
+const analysis = document.querySelector('#analysis')
