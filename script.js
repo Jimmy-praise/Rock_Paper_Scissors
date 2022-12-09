57f9aeaa9fdb11d1f74652f4586b1b98657f6eb7
Home_Laptop <Jimmlius923@gmail.com>
2022-12-09 07:29:12 +0100

    Added code to count scores and round

--- script.js
+++ script.js
@@ -15,46 +15,76 @@ function getComputerChoice() {
     return computerChoice;
 };
 
+const resultPanel = document.querySelector('.result-panel');
+const result = document.querySelector('#result')
+const analysis = document.querySelector('#analysis')
+
 function playRound(playerSelection) {
     computerSelection = getComputerChoice();
 
     if (computerSelection == playerSelection) {
-        resultPanel.classList.add('draw');
+        resultPanel.style.background = 'orange';
         result.textContent = 'NO WINNER!';
         analysis.textContent = `${computerSelection} cannot beat ${playerSelection}`;
-
+        return 'IT\'S A DRAW'
     }
     else if (
         computerSelection == 'ROCK' && playerSelection == "SCISSORS" || 
         computerSelection == 'SCISSORS' && playerSelection == "PAPER" || 
         computerSelection == 'PAPER' && playerSelection == "ROCK") {
-            resultPanel.classList.add('lose');
+            resultPanel.style.background = 'red';
             result.textContent = 'YOU LOSE!';
             analysis.textContent = `${playerSelection} cannot beat ${computerSelection}`;
+            return 'YOU LOSE'
     } 
     else {
-        resultPanel.classList.add('win');
+        resultPanel.style.background = 'green';
         result.textContent = 'YOU WON!';
         analysis.textContent = `${playerSelection} beats ${computerSelection}`;
+        return 'YOU WON';
     }
+}
+
+const currentRound = document.querySelector('#currentRound')
+const playerScore = document.querySelector('#playerScore')
+const computerScore = document.querySelector('#computerScore')
+
+let round = 1
+let wins = 0
+let lose = 0
+
+// Game
+function game(playerSelection) {
+    let gameround = playRound(playerSelection);
+    
+    if (gameround.includes('YOU WON')) {
+        wins += 1;
+        playerScore.textContent = wins;
+    } else if (gameround.includes('YOU LOSE')) {
+        lose += 1
+        computerScore.textContent = lose;
+    } else {
+
+    }
+    round += 1;
+    currentRound.textContent = round;
 }
 
 
 const rock = document.querySelector('#rock')
 const paper = document.querySelector('#paper')
 const scissors = document.querySelector('#scissors')
-
+    
     rock.addEventListener('click', () => {
-        playRound('ROCK')
+        let playerSelection = 'ROCK';
+        game(playerSelection);
     });
     paper.addEventListener('click', () => {
-        playRound('PAPER')
+        let playerSelection = 'PAPER';
+        game(playerSelection);
     });
     scissors.addEventListener('click', () => {
-        playRound('SCISSORS')
-    });
+        let playerSelection = 'SCISSORS'
+        game(playerSelection);
+    }); 
 
-
-const resultPanel = document.querySelector('.result-panel');
-const result = document.querySelector('#result')
-const analysis = document.querySelector('#analysis')
